9992295e1d228c0146d472b8bdb29faa90616471
Smax Laptop <home.maxi.schmidt@gmail.com>
2023-09-27 11:24:27 +0200

    progress

---
+++ SubnettingCalculator.Tests/NetworkTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubnettingCalculator.Models.Tests
+{
+    [TestFixture]
+    public class NetworkTests
+    {
+        [TestCase(new byte[] { 192, 168, 0, 1 }, new byte[] { 255, 255, 255, 0 })]
+        public void Should_CalcNetId(byte[] ipAddress, byte[] subnetMask)
+        {
+            var result = 
+        }
+    }
+}
