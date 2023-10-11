7a5ea34b6a0c5a9c74eb13973aacb87c0c4bcd01
Smax Laptop <home.maxi.schmidt@gmail.com>
2023-10-11 11:18:34 +0200

    MVVM initialized

--- SubnettingCalculator.ViewModels/MainViewModel.cs
+++ SubnettingCalculator.ViewModels/MainViewModel.cs
@@ -1,6 +1,41 @@
-﻿namespace SubnettingCalculator.ViewModels;
+﻿using SubnettingCalculator.ViewModels.Core;
 
-public class MainViewModel
+namespace SubnettingCalculator.ViewModels;
+
+public class MainViewModel : ObservableObject
 {
+    public RelayCommand IpCalculatorViewCommand { get; set; }
+    public RelayCommand SubnettingViewCommand { get; set; }
+    public IpCalculatorViewModel IpCalculatorViewModel { get; set; }
+    public SubnettingViewModel SubnettingViewModel { get; set; }
 
+    private object _currentView;
+
+	public object CurrentView
+	{
+		get { return _currentView; }
+		set 
+        { 
+            _currentView = value;
+            OnPropertyChanged();
+        }
+	}
+
+    public MainViewModel()
+    {
+        IpCalculatorViewModel = new IpCalculatorViewModel();
+        SubnettingViewModel = new SubnettingViewModel();
+
+        CurrentView = IpCalculatorViewModel;
+
+        IpCalculatorViewCommand = new RelayCommand(x => 
+        {
+            CurrentView = IpCalculatorViewModel;
+        });
+
+        SubnettingViewCommand = new RelayCommand(x =>
+        {
+            CurrentView = SubnettingViewModel;
+        });
+    }
 }
