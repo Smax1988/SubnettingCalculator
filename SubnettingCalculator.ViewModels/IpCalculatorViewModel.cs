023940c37700c3c813db8405b7717f4ff7336d47
Smax Laptop <home.maxi.schmidt@gmail.com>
2023-10-18 09:22:47 +0200

    ipcalculator viewmodel and view

--- SubnettingCalculator.ViewModels/IpCalculatorViewModel.cs
+++ SubnettingCalculator.ViewModels/IpCalculatorViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using SubnettingCalculator.Models;
+using SubnettingCalculator.ViewModels.Core;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,104 @@ using System.Threading.Tasks;
 
 namespace SubnettingCalculator.ViewModels
 {
-    public class IpCalculatorViewModel
+    public class IpCalculatorViewModel : ObservableObject
     {
 
+        private Network _network;
+
+        public Network Network
+        {
+            get { return _network; }
+            set
+            {
+                _network = value;
+                OnPropertyChanged();
+            }
+        }
+
+        //    private IpAddress _ipAddress;
+
+        //    public IpAddress IpAddress
+        //    {
+        //        get { return _ipAddress; }
+        //        set 
+        //        { 
+        //            _ipAddress = value;
+        //            OnPropertyChanged();
+        //        }
+        //    }
+
+        //    private SubnetMask _subnetMask;
+
+        //    public SubnetMask MyProperty
+        //    {
+        //        get { return _subnetMask; }
+        //        set 
+        //        { 
+        //            _subnetMask = value; 
+        //            OnPropertyChanged();
+        //        }
+        //    }
+
+        //    private IpAddress _broadcastAddress;
+
+        //    public IpAddress BroadcastAddress
+        //    {
+        //        get { return _broadcastAddress; }
+        //        set 
+        //        { 
+        //            _broadcastAddress = value;
+        //            OnPropertyChanged();
+        //        }
+        //    }
+
+        //    private IpAddress _firstHost;
+
+        //    public IpAddress FirstHost
+        //    {
+        //        get { return _firstHost; }
+        //        set 
+        //        { 
+        //            _firstHost = value; 
+        //            OnPropertyChanged();
+        //        }
+        //    }
+
+        //    private IpAddress _lastHost;
+
+        //    public IpAddress LastHost
+        //    {
+        //        get { return _lastHost; }
+        //        set 
+        //        { 
+        //            _lastHost = value; 
+        //            OnPropertyChanged();
+        //        }
+        //    }
+
+        //    private IpAddress _netId;
+
+        //    public IpAddress NetId
+        //    {
+        //        get { return _netId; }
+        //        set 
+        //        { 
+        //            _netId = value; 
+        //            OnPropertyChanged();
+        //        }
+        //    }
+
+        //    private int _numberOfHosts;
+
+        //    public int NumberOfHosts
+        //    {
+        //        get { return _numberOfHosts; }
+        //        set 
+        //        { 
+        //            _numberOfHosts = value; 
+        //            OnPropertyChanged();
+        //        }
+        //    }
+        //}
     }
 }
