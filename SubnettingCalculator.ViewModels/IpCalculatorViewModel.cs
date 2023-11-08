a1e212c35f572c3e794b7996470420eaff2d1972
Smax Laptop <home.maxi.schmidt@gmail.com>
2023-11-08 10:11:48 +0100

    warning message, still a little bug

--- SubnettingCalculator.ViewModels/IpCalculatorViewModel.cs
+++ SubnettingCalculator.ViewModels/IpCalculatorViewModel.cs
@@ -6,8 +6,9 @@ namespace SubnettingCalculator.ViewModels;
 public class IpCalculatorViewModel : ObservableObject
 {
     private bool _snmWarning = false;
-    private string _warning = string.Empty;
     private bool _ipWarning = false;
+    private bool _sfxWarning = false;
+    private string _warning = string.Empty;
     private int _cidrSuffix;
     private string _ipAddress;
     private string _subnetMask;
@@ -33,11 +34,18 @@ public class IpCalculatorViewModel : ObservableObject
             try
             {
                 Network = new Network(new IpAddress(IpAddress), new SubnetMask(SubnetMask));
+                _ipWarning = false;
             }
             catch (Exception)
             {
-                Warning = "Achtung! Ungültige IP Adresse.";
+                if (!_ipWarning) 
+                    Warning += "Ungültige IP Adresse. \r\n";
+                _ipWarning = true;
             }
+
+            if (!_ipWarning && !_snmWarning && !_sfxWarning)
+                Warning = string.Empty;
+
             OnPropertyChanged();
         }
     }
@@ -55,12 +63,17 @@ public class IpCalculatorViewModel : ObservableObject
                 Network = new Network(new IpAddress(IpAddress), new SubnetMask(SubnetMask));
                 CidrSuffix = Network.SubnetMask.CidrSuffix;
                 _snmWarning = false;
-                if (!_snmWarning && !_ipWarning) Warning = "";
             }
             catch (Exception)
             {
-                Warning += "Achtung! Ungültige Subnetzmaske";
+                if (!_snmWarning) 
+                    Warning += "Ungültige Subnetzmaske. \r\n";
+                _snmWarning = true;
             }
+
+            if (!_snmWarning && !_ipWarning && !_sfxWarning) 
+                Warning = string.Empty;
+
             OnPropertyChanged(); 
         }
     }
@@ -72,9 +85,24 @@ public class IpCalculatorViewModel : ObservableObject
         set 
         {
             if (value == _cidrSuffix) return;
-            _cidrSuffix = value; 
-            Network = new Network(new IpAddress(_ipAddress), new SubnetMask(_cidrSuffix));
-            SubnetMask = Network.SubnetMask.ToString();
+            _cidrSuffix = value;
+
+            try
+            {
+                Network = new Network(new IpAddress(_ipAddress), new SubnetMask(_cidrSuffix));
+                SubnetMask = Network.SubnetMask.ToString();
+                _sfxWarning = false;
+            }
+            catch (Exception)
+            {
+                if (!_sfxWarning)
+                    Warning += "Ungültiges Cidr Suffix. Suffix muss mindestens 1 und höchsten 31 sein. \r\n";
+                _sfxWarning = true;
+            }
+
+            if (!_sfxWarning && !_ipWarning && _snmWarning)
+                Warning = string.Empty;
+
             OnPropertyChanged();
         }
     }
